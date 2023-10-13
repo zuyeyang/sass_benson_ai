4a9c7aaac056ee1f5cd9aa73b33e3392a3741fb5
zuyeyang <zuyeyang@gmail.com>
2023-10-12 20:24:15 -0700

    first commit

---
+++ components/sidebar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative h-8 w-8 mr-4">
+            <Image fill alt="Logo" src="/logo.png" />
+          </div>
+          <h1 className={(cn("text-2xl font-bold"), montserrat.className)}>
+            Benson.ai
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
